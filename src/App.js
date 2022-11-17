1d0e398159722f814e1e897d3eabd3698d29b072
Raivals <103427644+Raivals@users.noreply.github.com>
2022-11-17 11:10:48 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Déclaration de Usestate
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [geoLoc, setGeoLoc] = useState({ latitude: 0, longitude: 0 })
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    if (!navigator.geolocation) {
+      window.alert(
+        "Votre navigateur ne permet pas la géolocalisation pour utiliser cette application !",
+      )
+    }
+    // Récup les données GPS
+    getGeolocalisation()
+
+    // Récup les données depuis l'API
+    fetchWeather(
+      "https://api.open-meteo.com/v1/forecast?latitude=48.68&longitude=6.15&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,rain_sum,windspeed_10m_max&timezone=Europe%2FLondon",
+    )
+  }, [])
+
+  // Données de Géoloc
+  const getGeolocalisation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setGeoLoc({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      },
+      () => {
+        setError(true)
+      },
+    )
+  }
+
+  // Récup les données depuis l'API
+  const fetchWeather = async (url) => {
+    setError(false)
+
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+
+      console.log(data)
+    } catch (error) {}
+  }
+
+  //
+  return <div className="App">Test</div>
 }
 
-export default App;
+export default App
